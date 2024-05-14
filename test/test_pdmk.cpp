5e589952fbedae43497b992d7002bb98a061acd4
Robert Blackwell <rblackwell@flatironinstitute.org>
2024-05-14 12:10:37 -0400

    cpp: spruce up the pdmk test

--- test/test_pdmk.cpp
+++ test/test_pdmk.cpp
@@ -1,45 +1,60 @@
 #include <dmk.h>
-#include <limits>
 #include <mpi.h>
 
 #include <cassert>
 #include <cmath>
 #include <cstdlib>
+#include <limits>
+#include <random>
 #include <vector>
 
-#include <dmk/fortran.h>
+void init_data(int n_dim, int nd, int n_src, bool uniform, std::vector<double> &r_src, std::vector<double> &rnormal,
+               std::vector<double> &charges, std::vector<double> &dipstr, long seed) {
+    r_src.resize(n_dim * n_src);
+    charges.resize(nd * n_src);
+    rnormal.resize(n_dim * n_src);
+    dipstr.resize(nd * n_src);
 
-void init_data(int n_dim, int nd, int n_src, bool uniform, double *src, double *rnormal, double *charges,
-               double *dipstr) {
     double rin = 0.45;
     double wrig = 0.12;
     double rwig = 0;
     int nwig = 6;
-    int zero = 0;
+
+    std::default_random_engine eng(seed);
+    std::uniform_real_distribution<double> rng;
 
     for (int i = 0; i < n_src; ++i) {
         if (!uniform) {
-            double theta = hkrand_(&zero) * M_PI;
-            double rr = rin + rwig * cos(nwig * theta);
-            double ct = cos(theta);
-            double st = sin(theta);
-            double phi = hkrand_(&zero) * 2 * M_PI;
-            double cp = cos(phi);
-            double sp = sin(phi);
-
-            if (n_dim == 3) {
-                src[i * 3 + 0] = rr * st * cp + 0.5;
-                src[i * 3 + 1] = rr * st * sp + 0.5;
-                src[i * 3 + 2] = rr * ct + 0.5;
+            if (n_dim == 2) {
+                double phi = rng(eng) * 2 * M_PI;
+                r_src[i * 3 + 0] = cos(phi);
+                r_src[i * 3 + 1] = sin(phi);
             }
+            if (n_dim == 3) {
+                double theta = rng(eng) * M_PI;
+                double rr = rin + rwig * cos(nwig * theta);
+                double ct = cos(theta);
+                double st = sin(theta);
+                double phi = rng(eng) * 2 * M_PI;
+                double cp = cos(phi);
+                double sp = sin(phi);
+
+                r_src[i * 3 + 0] = rr * st * cp + 0.5;
+                r_src[i * 3 + 1] = rr * st * sp + 0.5;
+                r_src[i * 3 + 2] = rr * ct + 0.5;
+            }
+        }
+        else {
+            for (int j = 0; j < n_dim; ++j)
+                r_src[i * n_dim + j] = rng(eng);
         }
 
         for (int j = 0; j < n_dim; ++j)
-            rnormal[i * n_dim + j] = hkrand_(&zero);
+            rnormal[i * n_dim + j] = rng(eng);
 
         for (int j = 0; j < nd; ++j) {
-            charges[i * nd + j] = hkrand_(&zero) - 0.5;
-            dipstr[i * nd + j] = hkrand_(&zero);
+            charges[i * nd + j] = rng(eng) - 0.5;
+            dipstr[i * nd + j] = rng(eng);
         }
     }
 
@@ -52,35 +67,64 @@ void init_data(int n_dim, int nd, int n_src, bool uniform, double *src, double *
     }
 
     for (int i = 0; i < 3; ++i)
-        src[i] = 0.0;
+        r_src[i] = 0.0;
     for (int i = 3; i < 6; ++i)
-        src[i] = 1 - std::numeric_limits<double>::epsilon();
+        r_src[i] = 1 - std::numeric_limits<double>::epsilon();
     for (int i = 6; i < 9; ++i)
-        src[i] = 0.05;
+        r_src[i] = 0.05;
+}
+
+std::vector<double> scatter(const std::vector<double> root, int dim) {
+    int rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int N = root.size() / dim;
+    int N_local = N / world_size + (rank < (N % world_size));
+
+    std::vector<int> counts(world_size);
+    std::vector<int> displs(world_size + 1);
+    for (int i = 0; i < world_size; ++i) {
+        counts[i] = (N / world_size + (i < (N % world_size))) * dim;
+        displs[i + 1] = displs[i] + counts[i];
+    }
+
+    MPI_Bcast(&N_local, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    std::vector<double> res(N_local * dim);
+    MPI_Scatterv(root.data(), counts.data(), displs.data(), MPI_DOUBLE, res.data(), res.size(), MPI_DOUBLE, 0,
+                 MPI_COMM_WORLD);
+    return res;
 }
 
 int main(int argc, char *argv[]) {
-    int req = MPI_THREAD_SERIALIZED, prov;
+    int req = MPI_THREAD_SERIALIZED, prov, rank, world_size;
     MPI_Init_thread(&argc, &argv, req, &prov);
+
     assert(prov == req);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     constexpr int n_dim = 3;
-    constexpr int n_src_per_rank = 1e6;
-    constexpr int n_trg_per_rank = 0;
+    constexpr int n_src = 1e6;
+    constexpr int n_trg = 0;
     constexpr int nd = 1;
-    std::vector<double> X(n_dim * n_src_per_rank);
-    std::vector<double> charges(nd * n_src_per_rank);
-    std::vector<double> rnormal(n_dim * n_src_per_rank);
-    std::vector<double> dipstr(nd * n_src_per_rank);
-    std::vector<double> pot(nd * n_src_per_rank);
-    std::vector<double> r_trg(n_dim * n_trg_per_rank);
+    int n_src_local = n_src / world_size + (rank < (n_src % world_size));
+    int n_trg_local = n_trg / world_size + (rank < (n_trg % world_size));
 
-    init_data(n_dim, 1, n_src_per_rank, false, X.data(), rnormal.data(), charges.data(), dipstr.data());
+    std::vector<double> X_root, charges_root, rnormal_root, dipstr_root, pot_root, r_trg_root;
+    if (rank == 0)
+        init_data(n_dim, 1, n_src, true, X_root, rnormal_root, charges_root, dipstr_root, 0);
+
+    auto r_src = scatter(X_root, n_dim);
+    auto r_trg = scatter(r_trg_root, n_dim);
+    auto rnormal = scatter(rnormal_root, n_dim);
+    auto charges = scatter(charges_root, nd);
+    auto dipstr = scatter(dipstr_root, nd);
+    std::vector<double> pot(nd * n_src_local);
 
     pdmk_params params;
     params.eps = 1e-6;
     params.n_dim = n_dim;
-    params.n_per_leaf = 80;
+    params.n_per_leaf = 10000;
     params.n_mfm = nd;
     params.pgh = DMK_POTENTIAL;
     params.kernel = DMK_YUKAWA;
@@ -88,9 +132,9 @@ int main(int argc, char *argv[]) {
     params.use_dipole = false;
     params.log_level = 0;
 
-    pdmk(params, n_src_per_rank, X.data(), charges.data(), rnormal.data(), dipstr.data(), n_trg_per_rank, r_trg.data(),
+    pdmk(params, n_src_local, r_src.data(), charges.data(), rnormal.data(), dipstr.data(), n_trg_local, r_trg.data(),
          pot.data(), nullptr, nullptr, nullptr, nullptr, nullptr);
-    pdmk(params, n_src_per_rank, X.data(), charges.data(), rnormal.data(), dipstr.data(), n_trg_per_rank, r_trg.data(),
+    pdmk(params, n_src_local, r_src.data(), charges.data(), rnormal.data(), dipstr.data(), n_trg_local, r_trg.data(),
          pot.data(), nullptr, nullptr, nullptr, nullptr, nullptr);
 
     MPI_Finalize();
