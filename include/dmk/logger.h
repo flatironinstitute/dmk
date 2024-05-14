1ad0c2e74d9205734baaa130effbf851e2e73389
Robert Blackwell <rblackwell@flatironinstitute.org>
2024-05-14 10:05:15 -0400

    cpp: add morton support to spdlog

--- include/dmk/logger.h
+++ include/dmk/logger.h
@@ -1,6 +1,7 @@
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
+#include <sctl.hpp>
 #include <spdlog/spdlog.h>
 
 namespace dmk {
@@ -9,5 +10,23 @@ std::shared_ptr<spdlog::logger> &get_logger(int level);
 std::shared_ptr<spdlog::logger> &get_rank_logger();
 std::shared_ptr<spdlog::logger> &get_rank_logger(int level);
 } // namespace dmk
+
+template <>
+struct fmt::formatter<sctl::Morton<2>> : fmt::formatter<std::string> {
+    auto format(sctl::Morton<2> my, format_context &ctx) const -> decltype(ctx.out()) {
+        std::stringstream stream;
+        stream << my;
+        return format_to(ctx.out(), stream.str());
+    }
+};
+
+template <>
+struct fmt::formatter<sctl::Morton<3>> : fmt::formatter<std::string> {
+    auto format(sctl::Morton<3> my, format_context &ctx) const -> decltype(ctx.out()) {
+        std::stringstream stream;
+        stream << my;
+        return format_to(ctx.out(), stream.str());
+    }
+};
 
 #endif
