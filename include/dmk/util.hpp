f2b3654c7e661386c9fc071aa5278e916ec6c535
amanrani <aman.rani@ttu.edu>
2024-07-16 11:24:41 -0400

    minor updates

--- include/dmk/util.hpp
+++ include/dmk/util.hpp
@@ -2,6 +2,7 @@
 #define UTIL_HPP
 
 #include <type_traits>
+#include <dmk/types.hpp>
 
 namespace dmk::util {
 template <class...>
@@ -9,6 +10,9 @@ constexpr std::false_type always_false{};
 
 template <typename Real>
 void mesh_nd(int dim, Real *in, int size, Real *out);
+
+template <typename Real>
+void mesh_nd(int dim, dmk::ndview<const Real, 1> &x, dmk::ndview<Real, 2> &out);
 } // namespace dmk::util
 
 #endif
