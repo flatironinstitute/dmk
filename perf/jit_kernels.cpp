1428039b045436212b62435bb01dd742a35c62f4
Robert Blackwell <rblackwell@flatironinstitute.org>
2025-12-10 11:59:16 -0500

    jit: add optional scale_factor back to evaluator

--- perf/jit_kernels.cpp
+++ perf/jit_kernels.cpp
@@ -1,5 +1,7 @@
 #include <sctl.hpp>
 
+#define ALWAYS_INLINE __attribute__((always_inline))
+
 template <typename T, size_t StackSize>
 class StackOrHeapBuffer {
     alignas(sizeof(T)) std::array<T, StackSize> stack_buffer_;
@@ -27,7 +29,7 @@ class StackOrHeapBuffer {
 };
 
 template <class F, class... Args>
-__attribute__((always_inline)) void dispatch_digits(int n_digits, F &&f, Args &&...args) {
+ALWAYS_INLINE void dispatch_digits(int n_digits, F &&f, Args &&...args) {
     switch (n_digits) {
     case 2:
         return f.template operator()<2>(std::forward<Args>(args)...);
@@ -58,8 +60,7 @@ __attribute__((always_inline)) void dispatch_digits(int n_digits, F &&f, Args &&
 
 // So far this almost always loses, and wwhen it wins, it's not by a lot
 template <typename VecType>
-__attribute__((always_inline)) VecType blocked_horner(const VecType &x, const typename VecType::ScalarType *coeffs,
-                                                      int n_coeffs) {
+ALWAYS_INLINE VecType blocked_horner(const VecType &x, const typename VecType::ScalarType *coeffs, int n_coeffs) {
     VecType x2 = x * x;
     VecType x4 = x2 * x2;
 
@@ -91,8 +92,7 @@ __attribute__((always_inline)) VecType blocked_horner(const VecType &x, const ty
 }
 
 template <typename VecType>
-__attribute__((always_inline)) VecType horner(const VecType &x, const typename VecType::ScalarType *coeffs,
-                                              int n_coeffs) {
+ALWAYS_INLINE VecType horner(const VecType &x, const typename VecType::ScalarType *coeffs, int n_coeffs) {
     VecType poly = VecType::Zero();
     for (int i = n_coeffs - 1; i >= 0; --i)
         poly = FMA(poly, x, VecType::Load1(&coeffs[i]));
@@ -107,6 +107,13 @@ void EvalPairs(int Ns, const Real *__restrict__ r_src, const Real *__restrict__ 
     using namespace sctl;
     static constexpr Integer digits_ = (digits == -1 ? (Integer)(TypeTraits<Real>::SigBits * 0.3010299957) : digits);
     using RealVec = Vec<Real, VecLen>;
+
+    Real scale_factor = 1.0;
+    if constexpr (requires {
+                      { uKernel.scale_factor() };
+                  }) {
+        scale_factor = static_cast<Real>(uKernel.scale_factor());
+    }
 
     const Long NNt = ((Nt + VecLen - 1) / VecLen) * VecLen;
     if (NNt == VecLen) {
@@ -146,8 +153,7 @@ void EvalPairs(int Ns, const Real *__restrict__ r_src, const Real *__restrict__ 
             alignas(sizeof(RealVec)) std::array<Real, VecLen> out;
             vt[k].StoreAligned(&out[0]);
             for (Long t = 0; t < Nt; t++) {
-                v_trg[t * KERNEL_OUTPUT_DIM + k] += out[t] // * uKernelEvaluator::template uKerScaleFactor<Real>()
-                    ;
+                v_trg[t * KERNEL_OUTPUT_DIM + k] += out[t] * scale_factor;
             }
         }
     } else {
@@ -193,10 +199,11 @@ void EvalPairs(int Ns, const Real *__restrict__ r_src, const Real *__restrict__ 
                 RealVec dX[SPATIAL_DIM], U[KERNEL_INPUT_DIM][KERNEL_OUTPUT_DIM];
                 for (Integer i = 0; i < SPATIAL_DIM; i++)
                     dX[i] = xt[i] - xs[i];
-                if constexpr (NORMAL_DIM > 0)
+                if constexpr (NORMAL_DIM > 0) {
                     uKernel(U, dX, ns);
-                else
+                } else {
                     uKernel(U, dX);
+                }
 
                 for (Integer k0 = 0; k0 < KERNEL_INPUT_DIM; k0++) {
                     for (Integer k1 = 0; k1 < KERNEL_OUTPUT_DIM; k1++) {
@@ -208,10 +215,9 @@ void EvalPairs(int Ns, const Real *__restrict__ r_src, const Real *__restrict__ 
                 vt[k].StoreAligned(&Vt_[k * NNt + t]);
         }
 
-        for (Long k = 0; k < KERNEL_OUTPUT_DIM; k++) { // v_trg += Vt_
+        for (Long k = 0; k < KERNEL_OUTPUT_DIM; k++) {
             for (Long i = 0; i < Nt; i++) {
-                v_trg[i * KERNEL_OUTPUT_DIM + k] += Vt_[k * NNt + i] // * scale_factor
-                    ;
+                v_trg[i * KERNEL_OUTPUT_DIM + k] += Vt_[k * NNt + i] * scale_factor;
             }
         }
     }
@@ -224,36 +230,41 @@ void laplace_pswf_all_pairs_jit_impl(int nd, Real rsc, Real cen, Real d2max, Rea
                                      const Real *r_trg, Real *pot) {
     using VecType = sctl::Vec<Real, MaxVecLen>;
 
-    VecType thresh2_vec = thresh2;
-    VecType d2max_vec = d2max;
-    VecType rsc_vec = rsc;
-    VecType cen_vec = cen;
+    struct Evaluator {
+        VecType thresh2_vec, d2max_vec, rsc_vec, cen_vec;
+        const Real *coeffs;
+        int n_coeffs;
 
-    auto evaluator = [thresh2_vec, d2max_vec, rsc_vec, cen_vec, coeffs, n_coeffs](VecType(&u)[1][1],
-                                                                                  const VecType(&dX)[3]) {
-        const VecType R2 = dX[0] * dX[0] + dX[1] * dX[1] + dX[2] * dX[2];
-        const auto mask = (R2 > thresh2_vec) & (R2 < d2max_vec);
+        static constexpr Real scale_factor() { return Real{1.0}; }
 
-        // Branch misses not worth it for lower precisions. Always do the polynomial expansion then
-        if constexpr (digits >= 6) {
-            // Early exit if no targets are in range
-            if (!sctl::mask_any(mask)) {
-                u[0][0] = VecType::Zero();
-                return;
+        Evaluator(VecType thresh2_vec, VecType d2max_vec, VecType rsc_vec, VecType cen_vec, const Real *coeffs,
+                  int n_coeffs)
+            : thresh2_vec(thresh2_vec), d2max_vec(d2max_vec), rsc_vec(rsc_vec), cen_vec(cen_vec), coeffs(coeffs),
+              n_coeffs(n_coeffs) {}
+
+        void operator()(VecType (&u)[1][1], const VecType (&dX)[3]) const {
+            const VecType R2 = dX[0] * dX[0] + dX[1] * dX[1] + dX[2] * dX[2];
+            const auto mask = (R2 > thresh2_vec) & (R2 < d2max_vec);
+
+            if constexpr (digits >= 6) {
+                if (!sctl::mask_any(mask)) {
+                    u[0][0] = VecType::Zero();
+                    return;
+                }
             }
-        }
-        const VecType Rinv = sctl::approx_rsqrt<digits>(R2, mask);
+            const VecType Rinv = sctl::approx_rsqrt<digits>(R2, mask);
 
-        // evaluate the PSWF kernel
-        const VecType x = sctl::FMA(R2, Rinv, cen_vec) * rsc_vec;
-        u[0][0] = horner(x, coeffs, n_coeffs) * Rinv;
+            const VecType x = sctl::FMA(R2, Rinv, cen_vec) * rsc_vec;
+            u[0][0] = horner(x, coeffs, n_coeffs) * Rinv;
+        }
     };
+
+    Evaluator evaluator(thresh2, d2max, rsc, cen, coeffs, n_coeffs);
 
     constexpr int KERNEL_INPUT_DIM = 1;
     constexpr int KERNEL_OUTPUT_DIM = 1;
     constexpr int SPATIAL_DIM = 3;
     constexpr int NORMAL_DIM = 0;
-    constexpr Real scale_factor = Real{1.0};
 
     EvalPairs<Real, digits, KERNEL_INPUT_DIM, KERNEL_OUTPUT_DIM, SPATIAL_DIM, NORMAL_DIM, MaxVecLen>(
         n_src, r_src, charge, nullptr, n_trg, r_trg, pot, evaluator);
