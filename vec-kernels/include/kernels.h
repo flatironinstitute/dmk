a6ec8694b628f67cd9774cf2d10ab31947cfb1db
Shidong Jiang <sjiang@flatironinstitute.org>
2026-06-01 16:10:16 -0400

    Add 3D Laplace dipole path to pdmk4

--- vec-kernels/include/kernels.h
+++ vec-kernels/include/kernels.h
@@ -10,6 +10,12 @@ extern "C" {
 void l3d_local_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg, const double* ztarg, const int32_t* nt,double* pot, const double* thresh);
 
 void l3d_near_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg, const double* ztarg, const int32_t* nt,double* pot);
+
+void l3d_local_kernel_directdp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* dipvec, const double* xtarg,const double* ytarg, const double* ztarg, const int32_t* nt,double* pot, const double* thresh);
+
+void l3d_local_kernel_directcp_coef_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg, const double* ztarg, const int32_t* nt, const int32_t* ncoefs, const double* coefs, double* pot, const double* thresh);
+
+void l3d_local_kernel_directdp_coef_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* dipvec, const double* xtarg,const double* ytarg, const double* ztarg, const int32_t* nt, const int32_t* ncoefs, const double* coefs, const int32_t* ncoefsd, const double* coefsd, double* pot, const double* thresh);
   
 void sl3d_local_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg, const double* ztarg, const int32_t* nt, double* pot, const double* thresh);
 
