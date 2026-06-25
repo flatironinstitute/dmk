e3216120938e1a5347a91c584be334a9881e2f5f
Robert Blackwell <rblackwell@flatironinstitute.org>
2026-06-25 09:51:37 -0400

    cleanup: remove rest of examples cruft

--- examples/test_ewald.cpp
+++
@@ -1,8 +0,0 @@
-/* Test: running pme_poisson3d_lagrange() in a cpp file by including the header. 
-Running the function through a C API */
-
-#include <ewald_total.h>
-
-int main(int argc, char *argv[]) {
-    return test_pme_poisson3d_lagrange(argc, argv);
-}
