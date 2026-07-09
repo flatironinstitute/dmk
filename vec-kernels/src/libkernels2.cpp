de51d1cb116a55ebf0cb1efb03930c310c5c803a
Robert Blackwell <rblackwell@flatironinstitute.org>
2026-07-09 12:47:52 -0400

    cleanup: actually sync esp/main vec-kernels (no clang-format)

--- vec-kernels/src/libkernels2.cpp
+++ vec-kernels/src/libkernels2.cpp
@@ -1,6 +1,6 @@
 #include <kernels.h>
-#include <l3d_laplace_dipole_kernel.hpp>
 #include <template-kernels2.hpp>
+#include <l3d_laplace_dipole_kernel.hpp>
 #define VECDIM 4
 
 #ifdef __AVX512F__
@@ -18,132 +18,66 @@ template void l3d_local_kernel_directcp_vec_cpp<float, VECDIM>(
 extern "C" {
 #endif
 
-void l3d_local_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                    const double *cen, const double *d2max, const double *sources, const int32_t *ns,
-                                    const double *charge, const double *xtarg, const double *ytarg, const double *ztarg,
-                                    const int32_t *nt, double *pot, const double *thresh) {
-    l3d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, d2max, sources, ns, charge, xtarg,
-                                                      ytarg, ztarg, nt, pot, thresh);
+void l3d_local_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot, const double* thresh){
+  l3d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot, thresh);
 }
 
-void l3d_near_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                   const double *cen, const double *bsizeinv, const double *d2max,
-                                   const double *sources, const int32_t *ns, const double *charge, const double *xtarg,
-                                   const double *ytarg, const double *ztarg, const int32_t *nt, double *pot) {
-    l3d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, bsizeinv, d2max, sources, ns, charge,
-                                                     xtarg, ytarg, ztarg, nt, pot);
+void l3d_near_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot){
+  l3d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, bsizeinv, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot);
 }
 
-void l3d_local_kernel_directdp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                    const double *cen, const double *d2max, const double *sources, const int32_t *ns,
-                                    const double *dipvec, const double *xtarg, const double *ytarg, const double *ztarg,
-                                    const int32_t *nt, double *pot, const double *thresh) {
-    l3d_local_kernel_directdp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, d2max, sources, ns, dipvec, xtarg,
-                                                      ytarg, ztarg, nt, pot, thresh);
+void l3d_local_kernel_directdp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* dipvec, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot, const double* thresh){
+  l3d_local_kernel_directdp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, d2max, sources, ns, dipvec,xtarg, ytarg, ztarg, nt, pot, thresh);
 }
 
-void l3d_local_kernel_directcp_coef_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits,
-                                         const double *rsc, const double *cen, const double *d2max,
-                                         const double *sources, const int32_t *ns, const double *charge,
-                                         const double *xtarg, const double *ytarg, const double *ztarg,
-                                         const int32_t *nt, const int32_t *ncoefs, const double *coefs, double *pot,
-                                         const double *thresh) {
-    l3d_local_kernel_directcp_coef_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, d2max, sources, ns, charge,
-                                                           xtarg, ytarg, ztarg, nt, ncoefs, coefs, pot, thresh);
+void l3d_local_kernel_directcp_coef_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, const int32_t* ncoefs, const double* coefs, double* pot, const double* thresh){
+  l3d_local_kernel_directcp_coef_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, ncoefs, coefs, pot, thresh);
 }
 
-void l3d_local_kernel_directdp_coef_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits,
-                                         const double *rsc, const double *cen, const double *d2max,
-                                         const double *sources, const int32_t *ns, const double *dipvec,
-                                         const double *xtarg, const double *ytarg, const double *ztarg,
-                                         const int32_t *nt, const int32_t *ncoefs, const double *coefs,
-                                         const int32_t *ncoefsd, const double *coefsd, double *pot,
-                                         const double *thresh) {
-    l3d_local_kernel_directdp_coef_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, d2max, sources, ns, dipvec,
-                                                           xtarg, ytarg, ztarg, nt, ncoefs, coefs, ncoefsd, coefsd, pot,
-                                                           thresh);
+void l3d_local_kernel_directdp_coef_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* dipvec, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, const int32_t* ncoefs, const double* coefs, const int32_t* ncoefsd, const double* coefsd, double* pot, const double* thresh){
+  l3d_local_kernel_directdp_coef_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, d2max, sources, ns, dipvec,xtarg, ytarg, ztarg, nt, ncoefs, coefs, ncoefsd, coefsd, pot, thresh);
+}
+  
+void sl3d_local_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot, const double* thresh){
+  sl3d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot, thresh);
 }
 
-void sl3d_local_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                     const double *cen, const double *d2max, const double *sources, const int32_t *ns,
-                                     const double *charge, const double *xtarg, const double *ytarg,
-                                     const double *ztarg, const int32_t *nt, double *pot, const double *thresh) {
-    sl3d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, d2max, sources, ns, charge, xtarg,
-                                                       ytarg, ztarg, nt, pot, thresh);
+void sl3d_near_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot){
+  sl3d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, bsizeinv, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot);
 }
 
-void sl3d_near_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                    const double *cen, const double *bsizeinv, const double *d2max,
-                                    const double *sources, const int32_t *ns, const double *charge, const double *xtarg,
-                                    const double *ytarg, const double *ztarg, const int32_t *nt, double *pot) {
-    sl3d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, bsizeinv, d2max, sources, ns, charge,
-                                                      xtarg, ytarg, ztarg, nt, pot);
+void log_local_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot, const double* thresh){
+  log_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot, thresh);
 }
 
-void log_local_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                    const double *cen, const double *d2max, const double *sources, const int32_t *ns,
-                                    const double *charge, const double *xtarg, const double *ytarg, const double *ztarg,
-                                    const int32_t *nt, double *pot, const double *thresh) {
-    log_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, d2max, sources, ns, charge, xtarg,
-                                                      ytarg, ztarg, nt, pot, thresh);
+void log_near_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot){
+  log_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, bsizeinv, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot);
 }
 
-void log_near_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                   const double *cen, const double *bsizeinv, const double *d2max,
-                                   const double *sources, const int32_t *ns, const double *charge, const double *xtarg,
-                                   const double *ytarg, const double *ztarg, const int32_t *nt, double *pot) {
-    log_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, bsizeinv, d2max, sources, ns, charge,
-                                                     xtarg, ytarg, ztarg, nt, pot);
+void y3ddirectcp_cpp_(const int32_t* nd, const int32_t* digits,const double* rlambda,const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot, const double* thresh){
+  y3ddirectcp_vec_cpp<double, VECDIM>(nd, digits,rlambda, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot, thresh);
 }
 
-void y3ddirectcp_cpp_(const int32_t *nd, const int32_t *digits, const double *rlambda, const double *d2max,
-                      const double *sources, const int32_t *ns, const double *charge, const double *xtarg,
-                      const double *ytarg, const double *ztarg, const int32_t *nt, double *pot, const double *thresh) {
-    y3ddirectcp_vec_cpp<double, VECDIM>(nd, digits, rlambda, d2max, sources, ns, charge, xtarg, ytarg, ztarg, nt, pot,
-                                        thresh);
+void st3d_local_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double *d2min, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot){
+  st3d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, bsizeinv, d2min, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot);
 }
 
-void st3d_local_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                     const double *cen, const double *bsizeinv, const double *d2min,
-                                     const double *d2max, const double *sources, const int32_t *ns,
-                                     const double *charge, const double *xtarg, const double *ytarg,
-                                     const double *ztarg, const int32_t *nt, double *pot) {
-    st3d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, bsizeinv, d2min, d2max, sources, ns,
-                                                       charge, xtarg, ytarg, ztarg, nt, pot);
+void st3d_near_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double* d2min, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot){
+  st3d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, bsizeinv, d2min, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot);
 }
 
-void st3d_near_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                    const double *cen, const double *bsizeinv, const double *d2min, const double *d2max,
-                                    const double *sources, const int32_t *ns, const double *charge, const double *xtarg,
-                                    const double *ytarg, const double *ztarg, const int32_t *nt, double *pot) {
-    st3d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, bsizeinv, d2min, d2max, sources, ns,
-                                                      charge, xtarg, ytarg, ztarg, nt, pot);
+void st2d_local_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double *d2min, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot){
+  st2d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, bsizeinv, d2min, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot);
 }
 
-void st2d_local_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                     const double *cen, const double *bsizeinv, const double *d2min,
-                                     const double *d2max, const double *sources, const int32_t *ns,
-                                     const double *charge, const double *xtarg, const double *ytarg,
-                                     const double *ztarg, const int32_t *nt, double *pot) {
-    st2d_local_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, bsizeinv, d2min, d2max, sources, ns,
-                                                       charge, xtarg, ytarg, ztarg, nt, pot);
+void st2d_near_kernel_directcp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double* bsizeinv, const double* d2min, const double* d2max, const double* sources,const int32_t* ns, const double* charge, const double* xtarg,const double* ytarg,const double* ztarg,const int32_t* nt, double* pot){
+  st2d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, bsizeinv, d2min, d2max, sources, ns, charge,xtarg, ytarg, ztarg, nt, pot);
 }
 
-void st2d_near_kernel_directcp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                    const double *cen, const double *bsizeinv, const double *d2min, const double *d2max,
-                                    const double *sources, const int32_t *ns, const double *charge, const double *xtarg,
-                                    const double *ytarg, const double *ztarg, const int32_t *nt, double *pot) {
-    st2d_near_kernel_directcp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, bsizeinv, d2min, d2max, sources, ns,
-                                                      charge, xtarg, ytarg, ztarg, nt, pot);
+void log_local_kernel_directdp_cpp_(const int32_t* nd, const int32_t* ndim, const int32_t* digits,const double* rsc,const double* cen, const double *d2min, const double* d2max, const double* sources,const int32_t* ns, const double* dipvec, const double* xtarg,const double* ytarg,const int32_t* nt, double* pot){
+  log_local_kernel_directdp_vec_cpp<double, VECDIM>(nd, ndim, digits,rsc, cen, d2min, d2max, sources, ns, dipvec, xtarg, ytarg, nt, pot);
 }
 
-void log_local_kernel_directdp_cpp_(const int32_t *nd, const int32_t *ndim, const int32_t *digits, const double *rsc,
-                                    const double *cen, const double *d2min, const double *d2max, const double *sources,
-                                    const int32_t *ns, const double *dipvec, const double *xtarg, const double *ytarg,
-                                    const int32_t *nt, double *pot) {
-    log_local_kernel_directdp_vec_cpp<double, VECDIM>(nd, ndim, digits, rsc, cen, d2min, d2max, sources, ns, dipvec,
-                                                      xtarg, ytarg, nt, pot);
-}
 
 #ifdef __cplusplus
 }
